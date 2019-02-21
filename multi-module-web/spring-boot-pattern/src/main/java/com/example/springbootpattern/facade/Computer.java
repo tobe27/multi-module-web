2f5d179f38a456a65f7b599a08fee8c96180bbe4
lichunyu <lichunyu@yzsbank.com>
2019-02-21 14:19:59 +0800

    设计模式-外观（门面）模式

---
+++ multi-module-web/spring-boot-pattern/src/main/java/com/example/springbootpattern/facade/Computer.java
@@ -0,0 +1,9 @@
+package com.example.springbootpattern.facade;
+
+/**
+ * @author CREATED BY L.C.Y on 2019-2-21
+ */
+public interface Computer {
+    void startUp();
+    void shutdown() ;
+}
---
+++ multi-module-web/spring-boot-pattern/src/main/java/com/example/springbootpattern/facade/Disk.java
@@ -0,0 +1,14 @@
+package com.example.springbootpattern.facade;
+
+/**
+ * @author CREATED BY L.C.Y on 2019-2-21
+ */
+public class Disk {
+    public void startUp() {
+        System.out.println("启动 Disk");
+    }
+
+    public void shutdown() {
+        System.out.println("关闭 Disk");
+    }
+}
